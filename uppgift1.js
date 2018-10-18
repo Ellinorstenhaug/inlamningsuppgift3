7ceb0cd145c1b3a4516ec117c05b4c3802f6b7d0
EllinorStenhaug <ellinor.stenhaug@gmail.com>
2018-10-18 14:17:47 +0200

    Ny version

--- uppgift1.js
+++ uppgift1.js
@@ -1,19 +1,22 @@
-function Person(namn, email, mobil, adress) {
-    this.namn = namn;
-    this.email = email;
+function Person(name, epost, mobil, adress){
+    this.name = name;
+    this.epost = epost;
     this.mobil = mobil;
     this.adress = adress;
-    this.print = function () {
-        return (`${this.namn}\n\n${this.adress.gatuadress}\n${this.adress.postnummer} ${this.adress.ort}`);
+    this.print = function(){
+        return `${this.adress.gatuadress} sdf${this.adress.postnummer} 'sdf'${this.adress.ort} `;
+    
     }
 }
 
-function Adress(gatuadress, postnummer, ort) {
+let Adress = function (gatuadress, postnummer, ort){
     this.gatuadress = gatuadress;
     this.postnummer = postnummer;
     this.ort = ort;
-}
+    
+    }
 
-let ellinor = new Person("Ellinor Stenhaug", "Ellinor.Stenhaug@gmail.com", "073-6504717", new Adress("Strandskatevägen 4", "134 62", "Ingarö"));
+let ellinor = new Person("Ellinor", "ellinor.stenhaug@gmail.com", "0701233456", new Adress("Strandskatevägen 4","134 62", "Ingarö"));
+let mikaela = new Person("Mikaela", "mikaela_johansson@hotmail.com", "0701233456", new Adress("Åstorpsringen 19","121 32", "Enskededalen"));
+
 console.log(ellinor.print());
-document.write(ellinor.print());
