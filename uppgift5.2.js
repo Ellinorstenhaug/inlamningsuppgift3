e382e93a3a4b62b666c0e5e84328d7ba3b4196b7
MikaelaJ <mikaela_johansson@hotmail.com>
2018-10-30 13:00:22 +0100

    uppdates

--- uppgift5.2.js
+++ uppgift5.2.js
@@ -1,44 +1,25 @@
-function randomDice(){
-    let arrDice = [];
-    for (let i = 0; i < 10; i++) {
-        arrDice[i] = Math.floor((Math.random() * 6) +1); // arrDice[i] väljer plats i arrayen och tilldelas värdet av slumpmässiga tal genererat av ((Math.random() * 6) +1) Math.floor avrundar talet nedåt och visar ett heltal.
-    }    
-    return arrDice;
+function uppgift5_2() {
+    let ggr = prompt("Hur många gånger vill du kasta tärningen?");
+    alert("Mahmud kolla i konsolen \nGlad halloween!");
+
+    function randomDice() {
+        let arrDice = [];
+        let smallArr = new Array(6).fill(0); // Gör en Array med 6 platser för att spara värdet på varsin plats. Eftersom man inte kan en plats som är tom '' i arrayen så fyller man upp den med talet 0 med hjälp av metoden fill() som finns till array.
+        for (let i = 0; i < ggr; i++) { // Den här forloopen genererar slumpmässiga tal mellan 1-6 som repsenterar tärningsslagen. Antalet ggr man vill kasta tärningen läses in via prompt.
+            arrDice[i] = Math.floor((Math.random() * 6) + 1); // arrDice[i] väljer plats i arrayen och tilldelas värdet av slumpmässiga tal genererat av ((Math.random() * 6) +1) Math.floor avrundar talet nedåt och visar ett heltal.
+            let number = arrDice[i] // number sparar värdet som finns på index 'i' i arrDice.
+            switch (number) { // Beroende på vad number får för värde så kommer den plussa på den specifika platsen i smallArr.
+                case 1: smallArr[0]++; break;
+                case 2: smallArr[1]++; break;
+                case 3: smallArr[2]++; break;
+                case 4: smallArr[3]++; break;
+                case 5: smallArr[4]++; break;
+                case 6: smallArr[5]++; break;
+            }
+        }
+        console.log('Ettor\tTvåor\tTreor\tFyror\tFemmor\tSexor');
+        return `${smallArr[0]}\t${smallArr[1]}\t${smallArr[2]}\t${smallArr[3]}\t${smallArr[4]}\t${smallArr[5]}`
+    }
+    console.log(randomDice());
 }
-
-let number = arrDice
-switch (number) {
-    case 1:
-        newArr[0] + 1;
-        break;
-    case 2:
-        newArr[1] + 1;
-        break;
-    case 3:
-        newArr[2] + 1;
-        break;
-    case 4:
-        newArr[3] + 1;
-        break;
-    case 5:
-        newArr[4] + 1;
-        break;
-    case 6:
-        newArr[5] + 1;
-        break;
-    default:
-        title = 'test';
-        break;
-    
-}
-
-
-
-
-let arrSort = randomDice().sort();
-let lastIndex = arrSort.lastIndexOf(4) + 1;
-let firstIndex = arrSort.indexOf(4);
-console.log(arrSort);
-console.log(lastIndex);
-let arrSlice = arrSort.slice(firstIndex, lastIndex);
-console.log(arrSlice.length);
+uppgift5_2();
--- uppgift6.1.js
+++ uppgift6.1.js
@@ -1,13 +1,15 @@
-function mickis() {
-    let n = prompt('Skriv in ett nummer:')
+function uppgift6_2() {
+    let n = prompt('Skriv in det nummer som du vill beräkna fakultet på:')
     fakultet(n);
 
     function fakultet(n) {
-        if (n > 0 && n != isNaN(n)) {
-            return (n * (fakultet(n - 1)));
-        } else if (n == 0) {
+        if (n > 0) { // Den beräknar fakulteten för alla naturliga tal. Gränsen för javascripts MAX_VALUE (infinity) går vid 171!
+            return (n * (fakultet(n - 1))); // Använder rekursion dvs den anropar en funktion från sig själv! “self-calling-function”. Värdet för varje beräkning sparas i RAM-minnet och återanvänds. Den fungerar som en loop i det här fallet.
+        } else if (n == '0') { // Enligt definitionen av fakultet så är 0! = 1, vilket denna else if tar hand om.
             return 1;
-        } else {
+        } else if (n == '') { // Detta meddelar att det du skrivit in är tomt och att du måste skriva in ett nummer för att funktionen ska fungera.
+            return 'Du måste skriva in ett nummer.';
+        } else { // Här hamnar man om man skriver ogiltliga tal, minus-tal och strängar eller annat skräp.
             return 'Fakultet kan endast beräknas på tal större än 0, testa igen!';
         }
     }
