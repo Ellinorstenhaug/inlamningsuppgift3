043b26a588cde036d64658a985ebb366da9783c5
EllinorStenhaug <ellinor.stenhaug@gmail.com>
2018-10-30 13:00:37 +0100

    eli

--- uppgift5.js
+++ uppgift5.js
@@ -1,21 +1,16 @@
-// function randomDice() {
-//     return smallArr;
-// }
-// let arrSort = randomDice().sort();
-// let lastIndex = arrSort.lastIndexOf(2) + 1;
-// let firstIndex = arrSort.indexOf(2);
+//Denna lösning saknar en funktion, som efterfrågas i uppgiften. Dock är den snygg så vi tog med den ändå :-D
+function uppgift5() {
+    let ggr = prompt("Hur många gånger vill du kasta tärningen?");
+    alert("Mahmud! Kolla i konsolen \nGlad halloween🎃");
 
-// console.log(arrSort);
-// console.log(lastIndex);
-// let arrSlice = arrSort.slice(firstIndex, lastIndex);
-// console.log(arrSlice.length);
-
-let arrDice = [];
-let smallArr = [0,0,0,0,0,0];
-for (let i = 0; i < 10; i++) {
-    arrDice[i] = Math.floor((Math.random() * 6) + 1); // arrDice[i] väljer plats i arrayen och tilldelas värdet av slumpmässiga tal genererat av ((Math.random() * 6) +1) Math.floor avrundar talet nedåt och visar ett heltal.
-    value = arrDice[i];
-    +smallArr[value -1]++;
+    let arrDice = []; 
+    let smallArr = new Array(6).fill(0); 
+    for (let i = 0; i < ggr; i++) {
+        arrDice[i] = Math.floor((Math.random() * 6) + 1); // arrDice[i] väljer plats i arrayen och tilldelas värdet av slumpmässiga tal genererat av ((Math.random() * 6) +1) Math.floor avrundar talet nedåt och visar ett heltal.
+        let number = arrDice[i]; 
+        smallArr[number - 1]++; //När number är 1 ska den plussa på värdet på index 0 i smallArr. 
+    }
+    console.log('Ettor\tTvåor\tTreor\tFyror\tFemmor\tSexor');
+    let tabell = `${smallArr[0]}\t${smallArr[1]}\t${smallArr[2]}\t${smallArr[3]}\t${smallArr[4]}\t${smallArr[5]}`;
+    console.log(tabell);
 }
-console.log(arrDice);
-console.log(smallArr);
