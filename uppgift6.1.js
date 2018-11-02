a2a90db6c4f0fdae5b1b3059d3e2e94a8da367e6
MikaelaJ <mikaela_johansson@hotmail.com>
2018-11-02 15:34:09 +0100

    Max_value

--- uppgift6.1.js
+++ uppgift6.1.js
@@ -2,10 +2,13 @@ function uppgift6() {
     let n = prompt('Skriv in det nummer som du vill beräkna fakultet på:')
 
     function fakultet(n) {
-        if (n > 0 && n <= 170) { // Den beräknar fakulteten för alla naturliga tal. Gränsen för javascripts MAX_VALUE (infinity) går vid 171!
-            return (n * (fakultet(n - 1))); // Använder rekursion dvs den anropar en funktion från sig själv! “self-calling-function”. Värdet för varje beräkning sparas i RAM-minnet och återanvänds. Den fungerar som en loop i det här fallet.
-        } else if (n > 170){
-            alert('Du har nått javascripts MAX_VALUE!\nJavascipt klarar av att beräkna fakultet på max 170!.');
+        if (n > 0) { // Den beräknar fakulteten för alla naturliga tal. Gränsen för javascripts MAX_VALUE (infinity) går vid 171!
+            let result = (n * (fakultet(n - 1)));
+            if (result > Number.MAX_VALUE){
+                alert('Du har nått javascripts MAX_VALUE!\nJavascipt klarar av att beräkna fakultet på max 170!!'); // Använder rekursion dvs den anropar en funktion från sig själv! “self-calling-function”. Värdet för varje beräkning sparas i RAM-minnet och återanvänds. Den fungerar som en loop i det här fallet.
+            }else{
+                return result; // Använder rekursion dvs den anropar en funktion från sig själv! “self-calling-function”. Värdet för varje beräkning sparas i RAM-minnet och återanvänds. Den fungerar som en loop i det här fallet.
+            } 
         } else if (n == '0') { // Enligt definitionen av fakultet så är 0! = 1, vilket denna else if tar hand om.
             return 1;
         } else if (n == '') { // Detta meddelar att det du skrivit in är tomt och att du måste skriva in ett nummer för att funktionen ska fungera.
